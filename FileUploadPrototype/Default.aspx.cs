a9f6506596cb75188094240fb7de597088aaebed
Fuji <fuji.nguyen@workcontrol.com>
2022-09-04 13:14:36 -0400

    Update web client

--- FileUploadPrototype/Default.aspx.cs
+++ FileUploadPrototype/Default.aspx.cs
@@ -29,7 +29,9 @@ namespace FileUploadPrototype
         {
             List<FileUploadInfo> lisFileUpload = new List<FileUploadInfo>();
             var fileUploadInfo = new FileUploadInfo();
-            
+            //using (var fileUploadInfo = new FileUploadInfo())
+            //{
+            //}
             fileUploadInfo.FileName = FileUpload.FileName;
             fileUploadInfo.ContentType = FileUpload.ContentType;
             fileUploadInfo.ContentLength = FileUpload.ContentLength;
@@ -43,72 +45,19 @@ namespace FileUploadPrototype
         }
 
 
-        private bool DownloadFile(string fileName)
+        private void DownloadAttachment(string fileName)
         {
-            //File Path and File Name
-            string filePath = Server.MapPath(ConfigurationManager.AppSettings["UploadFilePath"]);
-
-            FileInfo fileInfo = new FileInfo(filePath + "\\" + fileName);
-            FileStream fileStream = new FileStream(filePath + "\\" + fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-
-            //Reads file as binary values
-            BinaryReader _BinaryReader = new BinaryReader(fileStream);
-
-            //Check whether file exists in specified location
-            if (fileInfo.Exists)
+            string path = Server.MapPath(ConfigurationManager.AppSettings["UploadFilePath"] + "\\" + fileName);
+            System.Net.WebClient client = new System.Net.WebClient();
+            Byte[] buffer = client.DownloadData(path);
+            if (buffer != null)
             {
-                try
-                {
-                    long startBytes = 0;
-                    string fileDate = File.GetLastWriteTimeUtc(filePath).ToString("r");
-                    string encodedData = HttpUtility.UrlEncode(fileName, Encoding.UTF8) + fileDate;
-
-                    Response.Clear();
-                    Response.Buffer = false;
-                    Response.AddHeader("Accept-Ranges", "bytes");
-                    Response.AppendHeader("ETag", "\"" + encodedData + "\"");
-                    Response.AppendHeader("Last-Modified", fileDate);
-                    Response.ContentType = "application/octect-stream";
-                    Response.AddHeader("Content-Disposition", "attachment;filename=" + fileInfo.Name);
-                    Response.AddHeader("Content-Length", (fileInfo.Length - startBytes).ToString());
-                    Response.AddHeader("Connection", "Keep-Alive");
-                    Response.ContentEncoding = Encoding.UTF8;
-
-                    //Read data
-                    _BinaryReader.BaseStream.Seek(startBytes, SeekOrigin.Begin);
-
-                    //Dividing the data in 1024 bytes package
-                    int maxCount = (int)Math.Ceiling((fileInfo.Length - startBytes + 0.0) / 1024);
-
-                    //Download in block of 1024 bytes
-                    int i;
-                    for (i = 0; i < maxCount && Response.IsClientConnected; i++)
-                    {
-                        Response.BinaryWrite(_BinaryReader.ReadBytes(1024));
-                        Response.Flush();
-                    }
-                    //if blocks transfered not equals total number of blocks
-                    if (i < maxCount)
-                        return false;
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
-                finally
-                {
-                    Response.End();
-                    _BinaryReader.Close();
-                    fileStream.Close();
-                }
+                Response.ContentType = "application/octect-stream";
+                Response.AddHeader("Content-Disposition", "attachment;filename=" + fileName);
+                Response.AddHeader("content-length", buffer.Length.ToString());
+                Response.BinaryWrite(buffer);
             }
-            else ScriptManager.RegisterStartupScript(this, GetType(),
-                "FileNotFoundWarning", "alert('File is not available now!')", true);
-
-            return false;
         }
-
 
         protected void btnFileUpload_Click(object sender, EventArgs e)
         {
@@ -122,7 +71,8 @@ namespace FileUploadPrototype
 
             //Get the value passed from gridview
             string fileName = lnkbtn.CommandArgument.ToString();
-            bool isSuccess = DownloadFile(fileName);
+
+            DownloadAttachment(fileName);
         }
     }
 }
